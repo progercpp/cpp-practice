5bfa24d311c5dfe7b356d39160ed753cd8caa569
diplay <diplaySuper@gmail.com>
2024-10-21 13:23:03 +0300

    advanced containers + string homework

---
+++ strings/palindrome/main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <cassert>
+#include "task.h"
+
+using namespace std;
+
+int main() {
+    assert(is_palindrome("abacaba"));
+    assert(is_palindrome("abaccaba"));
+    assert(!is_palindrome("abacdaba"));
+    assert(is_palindrome(""));
+    assert(is_palindrome("a"));
+    assert(is_palindrome("aa"));
+    assert(!is_palindrome("ab"));
+    assert(is_palindrome("a a"));
+    assert(is_palindrome("a A"));
+    assert(!is_palindrome("a b"));
+    assert(is_palindrome(" a "));
+    assert(is_palindrome(" a  "));
+    assert(is_palindrome("Do geese see God?"));
+    assert(is_palindrome("                ?"));
+    assert(is_palindrome(" z "));
+    assert(is_palindrome("@' Zaz [{"));
+    assert(!is_palindrome(" Zoom "));
+    return 0;
+}
