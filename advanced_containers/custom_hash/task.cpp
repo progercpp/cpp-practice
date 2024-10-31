d0d9949ac168d7cb91b5f041bb05d7bae28ac4e2
CTPAHHUK <egorolympiads@gmail.com>
2024-10-31 12:58:16 +0300

    2% of power

--- advanced_containers/custom_hash/task.cpp
+++ advanced_containers/custom_hash/task.cpp
@@ -2,8 +2,15 @@
 
 using namespace std;
 
+/*
+ * Напишите хэш-функцию, реализовав специализацию шаблона std::hash для типа SuperKey
+ * Напишите функцию PopulateHashMap, которая добавляет в hashMap пары ключ-значение из данного вектора toAdd
+ */
 void PopulateHashMap(
-    unordered_map<SuperKey, string>& hashMap,
-    const vector<pair<SuperKey, string>>& toAdd
+    std::unordered_map<SuperKey, std::string>& hashMap,
+    const std::vector<std::pair<SuperKey, std::string>>& toAdd
 ) {
+    for (auto [a, b] : toAdd) {
+        hashMap[a] = b;
+    }
 }
